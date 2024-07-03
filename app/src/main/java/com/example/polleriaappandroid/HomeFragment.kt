d66b66947e545a8a1ea4747c4a0e8dbce16e42fc
czfd27 <czfd27@gmail.com>
2024-07-03 17:23:29 -0500

    Se agrega al home fragment datos del producto

--- app/src/main/java/com/example/polleriaappandroid/HomeFragment.kt
+++ app/src/main/java/com/example/polleriaappandroid/HomeFragment.kt
@@ -1,103 +1,121 @@
 package com.example.polleriaappandroid
 
+import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.polleriaappandroid.databinding.FragmentHomeBinding
+import com.example.polleriaappandroid.databinding.ItemSnapshotBinding
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
 class HomeFragment : Fragment() {
 
-/*
+
+    private lateinit var mBinding: FragmentHomeBinding
+
+    private lateinit var mFirebaseAdapter: FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>
+
+    private lateinit var mLayoutManager: RecyclerView.LayoutManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return mBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val query = FirebaseDatabase.getInstance().reference.child("snapshots")
 
-    private lateinit var fBinding: FragmentHomeBinding
+        val options = FirebaseRecyclerOptions.Builder<Snapshot>()
+            .setQuery(query, Snapshot::class.java).build()
 
-    private lateinit var mAdapter: ProductoAdapter
+        mFirebaseAdapter = object : FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>(options) {
 
-    private lateinit var mGridLayout: GridLayoutManager
+            private lateinit var mContext: Context
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        fBinding = FragmentHomeBinding.inflate(layoutInflater)
-        setContentView(fBinding.root)
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolder {
+                mContext = parent.context
 
-        fBinding.btnSave.setOnClickListener {
-            val producto = ProductoEntity(name = fBinding.etName.text.toString().trim())
+                val view = LayoutInflater.from(mContext)
+                    .inflate(R.layout.item_snapshot, parent, false)
+                return SnapshotHolder(view)
+            }
 
-            // Se pone hilo para que se ejecute en segundo plano
-            Thread {
-                ProductoApplication.database.productoDao().addProducto(producto)
-            }.start()
-            // Agregando al Adaptador
-            mAdapter.add(producto)
+            override fun onBindViewHolder(holder: SnapshotHolder, position: Int, model: Snapshot) {
+                val snapshot = getItem(position)
+
+                with(holder) {
+                    setListener(snapshot)
+
+                    binding.tvNombre.text = snapshot.nombre
+                    binding.tvCategoria.text = snapshot.categoria
+                    binding.tvUnidadmedida.text = snapshot.unidadmedida
+                    binding.tvPresentacion.text = snapshot.presentacion
+                    binding.tvPrecio.text = snapshot.precio
+
+                    Glide.with(mContext)
+                        .load(snapshot.photoUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerCrop()
+                        .into(binding.imgPhoto)
+                }
+
+            }
+
+            override fun onDataChanged() {
+                super.onDataChanged()
+                mBinding.progressBar.visibility = View.GONE
+            }
+
+            override fun onError(error: DatabaseError) {
+                super.onError(error)
+                Toast.makeText(mContext, error.message, Toast.LENGTH_SHORT).show()
+            }
+
         }
-        setupRecyclerView()
 
-    }
+        mLayoutManager = LinearLayoutManager(context)
 
-    private fun setupRecyclerView() {
-        mAdapter = ProductoAdapter(mutableListOf(), this)
-        mGridLayout = GridLayoutManager(this, 2)
-        getProductos()
-
-        fBinding.recyclerView.apply {
+        mBinding.recyclerView.apply {
             setHasFixedSize(true)
-            layoutManager = mGridLayout
-            adapter = mAdapter
+            layoutManager = mLayoutManager
+            adapter = mFirebaseAdapter
         }
     }
 
-    private fun getProductos() {
-        val queue = LinkedBlockingQueue<MutableList<ProductoEntity>>()
-        Thread {
-            val productos = ProductoApplication.database.productoDao().getAllProductos()
-            queue.add(productos)
-        }.start()
-
-        // mAdapter.setProductos(productos)
-        mAdapter.setProductos(queue.take())
+    override fun onStart() {
+        super.onStart()
+        mFirebaseAdapter.startListening()
     }
 
-    override fun onClick(productoEntity: ProductoEntity) {
-
+    override fun onStop() {
+        super.onStop()
+        mFirebaseAdapter.stopListening()
     }
 
-    override fun onFavoriteProducto(productoEntity: ProductoEntity) {
-        productoEntity.isFavorite = !productoEntity.isFavorite
-        val queue = LinkedBlockingQueue<ProductoEntity>()
-        Thread {
-            ProductoApplication.database.productoDao().updateProducto(productoEntity)
-            queue.add(productoEntity)
-        }.start()
-        mAdapter.update(queue.take())
-    }
+    inner class SnapshotHolder(view: View): RecyclerView.ViewHolder(view) {
 
-    override fun onDeleteProducto(productoEntity: ProductoEntity) {
-        val queue = LinkedBlockingQueue<ProductoEntity>()
-        Thread {
-            ProductoApplication.database.productoDao().deleteProducto(productoEntity)
-            queue.add(productoEntity)
-        }.start()
-        mAdapter.delete(queue.take())
-    }
-*/
+        val binding = ItemSnapshotBinding.bind(view)
 
+        fun setListener(snapshot: Snapshot) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        }
+
     }
 
 
--- app/src/main/java/com/example/polleriaappandroid/OnClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.polleriaappandroid
-
-interface OnClickListener {
-
-    fun onClick(productoEntity: ProductoEntity)
-    fun onFavoriteProducto(productoEntity: ProductoEntity)
-    fun onDeleteProducto(productoEntity: ProductoEntity)
-}
